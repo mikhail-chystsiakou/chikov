83c6991797509d1fa636285e53e96d739d69f6dc
mich0217 <zoxal@pm.me>
2022-02-13 13:47:23 +0300

    Printing days of month

--- calendar/src/Calendar.java
+++ calendar/src/Calendar.java
@@ -1,7 +1,41 @@
 public class Calendar {
 
+    /**
+     * Пн Вт Ср Чт Пт Сб Вс
+     * 01 02 03 04 05 06 07
+     * 08 09 10 11 12 13 14
+     * 15 16 17 18 19 20 21
+     * 22 23 24 25 26 27 28
+     * 29 30 31
+     *
+     */
     public static void main(String[] args) {
-        String space = "   ";
+        int day = 1;
+
+        while (day < 32) {
+            String dayPrint = day + " ";
+
+            if (day < 10) {
+                dayPrint = "0" + dayPrint;
+            }
+
+//            System.out.printf("%02d ", day);
+
+
+            int d = day % 7;
+            if (d == 0) {
+                System.out.println();
+            }
+
+            day++;
+        }
+    }
+
+
+    public static void step6() {
+        String space; // объявление - define
+        space = "   ";// определение - definition
+
         String[] weekdays = {"Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"};
 
         int number = 0;
